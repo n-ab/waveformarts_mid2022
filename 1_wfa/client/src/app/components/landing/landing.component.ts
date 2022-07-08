c06973b8452af3e9846e34b86634ee4cd33c3de5
Nick Abercrombie <32076158+n-ab@users.noreply.github.com>
2022-07-08 13:26:49 -0500

    Moved animations into individual file, adjusted bg image functionality

--- 1_wfa/client/src/app/components/landing/landing.component.ts
+++ 1_wfa/client/src/app/components/landing/landing.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+      const landingFeature = document.getElementById('landing-feature');
+      setTimeout(() => {
+        landingFeature?.classList.remove('landing-feature-pre')
+        landingFeature?.classList.add('landing-feature-post');
+      }, 1500);
   }
 
 }
